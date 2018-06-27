6b48e3b0d366cdeb0e28e9fd0111775d7255c756
madycakes <viclee02@gmail.com>
2018-06-26 15:43:42 -1000

    adds exercises

---
+++ mission.js
@@ -0,0 +1,191 @@
+/*Congratulations, you've been selected for your final top secret mission. Should you choose to accept it, do a git fetch and a git pull to find your specific mission. After you complete your mission, commit your work and do a pull request.
+
+As always, should you be caught copy and pasta, the Secretary will disavow any knowledge of your actions. 
+This repo will self-destruct in five seconds. Good luck!*/
+
+/*Mission 1: 
+Team Member: 
+
+Create a function that will sum up only even numbers from 0 - 50.
+*/
+
+
+/*Mission 2: 
+Team Member: 
+
+Create a function that will sum up only odd numbers from 0 - 50.
+ */
+
+
+/*Mission 3: 
+Team Member: 
+
+Create a function that will sum up two numbers.
+ */
+
+
+/*Mission 4: 
+Team Member: 
+
+Create a function that will multiply two numbers.
+ */
+
+
+/*Mission 5:
+Team Member: 
+
+Create a function that will iterate through the given array and returns a new array with only the even indexed items.
+ */
+var myMenu = ['carrot cake', 'brocolli', 'lemon bars', 'spinach', 'oreo cheesecake', 'alfalfa sprouts', 'brownies'];
+
+
+/*Mission 6:
+Team Member: 
+
+Create a function that will sum up numbers from 0 - 100. 
+*/
+
+/*Mission 7:
+Team Member:
+
+Target the div element in the index.html and give it the content of "Hello World"
+*/
+
+
+/*Mission 8:
+Team Member: 
+
+Create a div element, give it an id of 'menu', give it the content of 'Aloooooha Everyone!' and append it to the body element.
+*/
+
+
+/*Mission 9:
+Team Member: 
+
+Create a function that will divide the following elements in the array by 2 and return a new array of your results.
+*/
+var numArr = [10, 20, 30, 40, 50];
+
+
+/*Mission 10:
+Team Member: 
+
+Create a function that will find the multiples of 5 from 0 - 100 and returns a new array of your results.
+*/
+
+
+/*Mission 11: 
+Team Member:
+
+@param Datatype: String
+@return Datatype: String
+
+Create a function named weatherReport that takes a parameter weather and returns the following: "Today's forecast is weather." Where your input will replace the weather parameter.
+Console.log to see your result.*/
+
+ 
+/*Mission 12: 
+Team Member: 
+ 
+@param Datatype: String
+@param Datatype: Number
+@return Datatype: String
+ 
+
+Create a function named happyBday that takes two parameters name and age and returns the following: "Happy Birthday name! You look awesome at age!" Where your input will replace the name and age parameters.
+Console log to see your result.*/
+ 
+ 
+/*Mission 13: 
+Team Member:
+ 
+@param Datatype: Array
+@return Datatype: Array
+ 
+Create a function named doubleUp that takes in a parameter arr and will double the value of each number in the array and returns the array.
+Console.log your result.*/
+ 
+ 
+/*Mission 14: 
+Team Member:
+ 
+@param Datatype: Array
+@return Datatype: Array
+ 
+Create a function named evens that takes in a parameter arr and will sum up only the even indexed numbers in the array and returns a new array.
+Console.log your result.*/
+ 
+
+/*Mission 15:
+Team Member: 
+ 
+@param Datatype: Array
+@param Datatype: String
+@param Datatype: Array
+ 
+Create a function named plusOne that takes two parameters arr and str and will add a new string element into an array and will return the array.
+Console.log your result*/
+ 
+
+/*Mission 16:
+Team Member:
+ 
+@param Datatype: Number
+@return Datatype: Number
+ 
+Create a function named yenConverter that takes a parameter rate and will convert dollars to yen based on today's foreign exchange rate and returns the new yen amount.
+Console.log your result*/
+ 
+
+/*Mission 17:
+Team Member: 
+ 
+@param Datatype: Number
+@return Datatype: Number
+ 
+Create a function named kmConverter that takes in a parameter miles and will convert miles to kilometers and returns the new kilometer amount
+Console.log your result.*/
+ 
+
+/*Mission 18:
+Team Member:
+ 
+@param Datatype: Number
+@return Datatype: String
+ 
+Create a function named whatToEat that takes in a parameter budget. Create the following return statements based on the budget amount: 
+ 
+ <=5 => "Mickey D's"
+ <=10 => "Panda Express"
+ >10 => "Peppas"
+ 
+Console.log your result*/
+ 
+
+/*Mission 19:
+Team Member:
+ 
+@param Datatype: Array
+@return Datatype: Array
+ 
+Create a function named countUp that takes in a parameter arr and will add 1 to each number in the array and returns the array.
+Console.log you result.*/
+var numBox = [6, 24, 16, 2, 3];
+ 
+
+/*Mission 20:
+Team Member:
+ 
+@param Datatype: Array
+@return Datatype: Array
+ 
+Create a function named backwards that takes in a parameter arr and returns the array in reverse order. Use a reverse for loop. 
+Console.log you result.*/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
